f75b2994ebf373899bee1acb9d7221b2818b1150
ooshiro3280611 <58359684+ooshiro3280611@users.noreply.github.com>
2020-02-25 13:08:16 +0900

    implement component with connect function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+
+// import './index.css';
+// import App from './components/App';
+// import * as serviceWorker from './serviceWorker';
+// import reducer from './reducers'
+// // import registerServiceWorker from './registerServiceWorker';
+
+// const store = createStore(reducer)
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+//     document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+// // registerServiceWorker();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import './index.css';
-import reducer from './reducers'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers'
 
 const store = createStore(reducer)
 
@@ -14,7 +40,8 @@ ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-    document.getElementById('root'));
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
