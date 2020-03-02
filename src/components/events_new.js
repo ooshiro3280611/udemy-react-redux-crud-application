7a0d09b1b2e608020e2be6835d805634ab2d1a8c
ooshiro3280611 <58359684+ooshiro3280611@users.noreply.github.com>
2020-03-02 22:56:32 +0900

    apply material-ui to eventsIndex

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,56 +1,80 @@
-import React, { Component, Suspense } from 'react';
-import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 
-import { postEvent } from '../actions'
+import { postEvent } from "../actions";
 
 class EventsNew extends Component {
   constructor(props) {
-    super(props)
-    this.onSubmit = this.onSubmit.bind(this)
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
   }
   renderField(field) {
-    const { input, label, type, meta: { touched, error } } = field
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error }
+    } = field;
 
     return (
       <div>
         <input {...input} placeholder={label} type={type} />
         {touched && error && <span>{error}</span>}
       </div>
-    )
+    );
   }
 
   async onSubmit(values) {
-    await this.props.postEvent(values)
-    this.props.history.push('/')
+    await this.props.postEvent(values);
+    this.props.history.push("/");
   }
 
   render() {
-    const { handleSubmit, pristine, submitting, invalid } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props;
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
-        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
-        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+        <div>
+          <Field
+            label="Title"
+            name="title"
+            type="text"
+            component={this.renderField}
+          />
+        </div>
+        <div>
+          <Field
+            label="Body"
+            name="body"
+            type="text"
+            component={this.renderField}
+          />
+        </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
+          <input
+            type="submit"
+            value="Submit"
+            disabled={pristine || submitting || invalid}
+          />
           <Link to="/">Cancel</Link>
         </div>
       </form>
-    )
+    );
   }
 }
 const validate = values => {
-  const errors = {}
+  const errors = {};
 
-  if (!values.title) errors.title = "Enter a title, please"
-  if (!values.body) errors.body = "Enter a body, please"
+  if (!values.title) errors.title = "Enter a title, please";
+  if (!values.body) errors.body = "Enter a body, please";
 
-  return errors
-}
-const mapDispatchToProps = ({ postEvent })
+  return errors;
+};
+const mapDispatchToProps = { postEvent };
 
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
-)
+export default connect(
+  null,
+  mapDispatchToProps
+)(reduxForm({ validate, form: "eventNewForm" })(EventsNew));
--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { composeWithDevTools } from "redux-devtools-extension";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-import './index.css';
-import EventsIndex from './components/events_index';
-import EventsNew from './components/events_new'
-import EventsShow from './components/events_show'
-import * as serviceWorker from './serviceWorker';
-import reducer from './reducers'
-const enhancer = process.env.NODE_ENV === 'development' ?
-    composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
-const store = createStore(reducer, enhancer)
+import "./index.css";
+import EventsIndex from "./components/events_index";
+import EventsNew from "./components/events_new";
+import EventsShow from "./components/events_show";
+import * as serviceWorker from "./serviceWorker";
+import reducer from "./reducers";
+const enhancer =
+  process.env.NODE_ENV === "development"
+    ? composeWithDevTools(applyMiddleware(thunk))
+    : applyMiddleware(thunk);
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
+  <MuiThemeProvider>
     <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route path="/events/new" component={EventsNew} />
-                <Route path="/events/:id" component={EventsShow} />
-                <Route exact path="/" component={EventsIndex} />
-                <Route exact path="/events" component={EventsIndex} />
-            </Switch>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want your EventsIndex to work offline and load faster, you can change
